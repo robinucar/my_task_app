44cd1aae0d773cdb36fa98c7712b117472a91403
Robin Winters <robinmelsaw@gmail.com>
2025-06-07 22:21:47 +0100

    feat: use express routes in main

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import taskRoutes from './routes/task.routes';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 5000;
 
@@ -8,6 +8,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
+
+app.use('/api/tasks', taskRoutes);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);

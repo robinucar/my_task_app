b92037998074338fd816799bc3e4cd2dd91a6188
Robin Winters <robinmelsaw@gmail.com>
2025-06-09 07:26:52 +0100

    feat: setup tanstackquery

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import App from './app/app';
+import { queryClient } from './lib/react-query-client';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
